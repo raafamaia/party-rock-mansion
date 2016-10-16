7c65db7db324d5117df251bfe22baf33e8b317a1
Maia <raafamaia@gmail.com>
2016-10-16 19:43:34 -0200

    layout

---
+++ party-rock-mansion/VideoVC.swift
@@ -0,0 +1,28 @@
+//
+//  VideoVC.swift
+//  party-rock-mansion
+//
+//  Created by R. Maia on 16/10/16.
+//  Copyright © 2016 RM. All rights reserved.
+//
+
+import UIKit
+
+class VideoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
